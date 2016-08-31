dc163999fe045eb50a55476013be8ed298bccf44
Gustavo Saavedra <glsaaved@uc.cl>
2016-08-31 15:22:04 -0300

    Update steps.js
    update steps.js

--- example/features/step_definitions/steps.js
+++ example/features/step_definitions/steps.js
@@ -2,11 +2,25 @@ var pc = require('../../../lib');
 
 var steps = function() {
   var seleniumAddress = 'http://localhost:4444/wd/hub';
-  var options = { browser : 'chrome', timeout : 100000 };
+  var options = { browser : 'chrome', timeout : 100000, uniqueBrowser:true };
   this.World = pc.world(seleniumAddress, options);
 
-  this.After(function(callback) {
-    this.quit(callback);
+  this.After(function (scenario, callback) {
+          if(scenario.isFailed()){
+              this.browser.takeScreenshot().then(function (buffer) {
+                 scenario.attach(new Buffer(buffer, 'base64'), 'image/png');
+              });
+          };
+          if(!options.uniqueBrowser){
+            this.quit(callback);
+          }
+          else{
+             callback();
+          }
+
+  });
+  this.After({tags: ["@final"]}, function (scenario, callback) {
+      this.quit(callback);
   });
 };
 
